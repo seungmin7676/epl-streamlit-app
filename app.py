066c8ed46c37e49abf627814c631b2795ff4553f
Lee Seungmin <lsm030706@naver.com>
2025-06-03 19:44:13 +0900

    t

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ if os.path.exists(font_path):
     fm.fontManager.addfont(font_path)
     font_prop = fm.FontProperties(fname=font_path)
     font_name = font_prop.get_name()
-
     plt.rcParams['font.family'] = font_name
     plt.rcParams['axes.unicode_minus'] = False
 else:
